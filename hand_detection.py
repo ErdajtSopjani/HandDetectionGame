20dcdc11b46b4370b0aabea191b891a6d205907b
ErdajtSopjani <120386306+ErdajtSopjani@users.noreply.github.com>
2023-04-16 01:35:38 +0200

    yes

--- hand_detection.py
+++ hand_detection.py
@@ -7,22 +7,22 @@ import random
  
  
 mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
+mp_duart = mp.solutions.hands
 score=0
  
-x_enemy=random.randint(50,600)
-y_enemy=random.randint(50,400)
+x_rrethi=random.randint(50,600)
+y_rrethi=random.randint(50,400)
  
  
  
-def enemy():
-  global score,x_enemy,y_enemy
-  cv2.circle(image, (x_enemy,y_enemy), 25, (0, 200, 0), 5)
+def rrethi():
+  global score,x_rrethi,y_rrethi
+  cv2.circle(image, (x_rrethi,y_rrethi), 25, (0, 200, 0), 5)
 
  
 video = cv2.VideoCapture(0)
  
-with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands: 
+with mp_duart.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands: 
     while video.isOpened():
         _, frame = video.read()
  
@@ -42,18 +42,18 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
         text=cv2.putText(image,"Score",(480,30),font,1,color,4,cv2.LINE_AA)
         text=cv2.putText(image,str(score),(590,30),font,1,color,4,cv2.LINE_AA)
  
-        enemy()
+        rrethi()
  
         if results.multi_hand_landmarks:
             for num, hand in enumerate(results.multi_hand_landmarks):
-                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS, 
+                mp_drawing.draw_landmarks(image, hand, mp_duart.HAND_CONNECTIONS, 
                                         mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
                                          )
  
  
         if results.multi_hand_landmarks != None:
           for handLandmarks in results.multi_hand_landmarks:
-            for point in mp_hands.HandLandmark:
+            for point in mp_duart.HandLandmark:
  
     
                 normalizedLandmark = handLandmarks.landmark[point]
@@ -65,16 +65,16 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
                  try:
                      cv2.circle(image, (pixelCoordinatesLandmark[0], pixelCoordinatesLandmark[1]), 25, (0, 200, 0), 5)
                      
-                     if pixelCoordinatesLandmark[0]==x_enemy or pixelCoordinatesLandmark[0]==x_enemy+10 or pixelCoordinatesLandmark[0]==x_enemy-10:
+                     if pixelCoordinatesLandmark[0]==x_rrethi or pixelCoordinatesLandmark[0]==x_rrethi+10 or pixelCoordinatesLandmark[0]==x_rrethi-10:
                        
                         print("found")
-                        x_enemy=random.randint(50,600)
-                        y_enemy=random.randint(50,400)
+                        x_rrethi=random.randint(50,600)
+                        y_rrethi=random.randint(50,400)
                         score=score+1
                         font=cv2.FONT_HERSHEY_SIMPLEX
                         color=(255,0,255)
                         text=cv2.putText(frame,"Score",(100,100),font,1,color,4,cv2.LINE_AA)
-                        enemy()
+                        rrethi|()
                  except:
                   pass
         
